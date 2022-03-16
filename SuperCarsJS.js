18955bed3503d3b395484398ef028127ddfbf358
Illyaas4Show <Illyaas123@outlook.com>
2022-03-16 20:34:18 +0000

    Initial commit

---
+++ SuperCarsJS.js
@@ -0,0 +1,7 @@
+var blocks = document.getElementsByClassName('quote-glow');
+var container = document.getElementsByClassName('quote');
+var hs = new HorizontalScroll.default({
+	blocks : blocks,
+	container: container,
+    isAnimated: true,
+});
