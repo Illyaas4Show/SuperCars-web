16aae66ebd6eb1127b6a2f7c3008709c37db3147
Illyaas4Show <Illyaas123@outlook.com>
2022-04-16 18:14:42 +0100

    News ticker(sorting)

--- SuperCarsJS.js
+++
@@ -1,6 +0,0 @@
-const scrollContainer = document.querySelector("quote");
-
-scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-});
